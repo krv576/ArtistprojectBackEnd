dfa560cf09ea82b48692e9728b00ced8497a2372
Rishik Vardhan <R.kolipaka@eagles.oc.edu>
2022-05-31 11:49:48 +0530

    Artist get all and get by id completed

--- app/controllers/artist.controller.js
+++ app/controllers/artist.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models");
 const Artist = db.artists;
+const Op = db.Sequelize.Op;
 // Create and Save a new Artist
 exports.create = (req, res) => {
   // Validate request
@@ -23,6 +24,40 @@ exports.create = (req, res) => {
       res.status(500).send({
         message:
           err.message || "Some error occurred while creating the Artist."
+      });
+    });
+};
+// Retrieve all Artists from the database.
+exports.findAll = (req, res) => {
+  const name = req.query.name;
+  var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
+  Artist.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving artists."
+      });
+    });
+};
+// Find a single Artist with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Artist.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Artist with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Artist with id=" + id
       });
     });
 };
